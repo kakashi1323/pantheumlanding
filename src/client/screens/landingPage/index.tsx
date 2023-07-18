3d25a958eb5de725a8fbc34d3d999aae7eb9e63b
TITANCORP\tai.tranth <tai.tranth@titancorpvn.com>
2023-07-18 14:57:31 +0700

    reset source

--- src/client/screens/landingPage/index.tsx
+++ src/client/screens/landingPage/index.tsx
@@ -5,15 +5,6 @@ import { memo, useEffect, useState } from "react"
 import PageBackground from "./components/PageBackground";
 import PageGraphic from "./components/PageGraphic";
 import PageContainer from "./components/PageContainer";
-import TopBar from "./components/TopBar";
-import Introduce from "./components/Introduce";
-import Branchs from "./components/Branchs";
-import What from "./components/What";
-import How from "./components/How";
-import Solutions from "./components/Solutions";
-import Cards from "./components/Cards";
-import GetMoreUpdates from "./components/GetMoreUpdates";
-import Footer from "./components/Footer";
 
 const LandingPage = (props: any) => {
 
@@ -33,15 +24,7 @@ const LandingPage = (props: any) => {
         <PageContainer>
           <PageGraphic>
             <div className="w-full flex flex-col">
-              <TopBar />
-              <Introduce />
-              <Branchs />
-              <What />
-              <How />
-              <Solutions />
-              <Cards />
-              <GetMoreUpdates />
-              <Footer />
+
             </div>
           </PageGraphic>
         </PageContainer>
